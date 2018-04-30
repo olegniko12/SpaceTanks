d69f91dd9e88e8b65a77b9b7db5166764903f8fe
olegniko12 <NikolaevO8292@sduhsd.lan>
2018-04-30 09:25:03 -0700

    SpaceTanks
    First version

---
+++ src/Battlefield.java
@@ -0,0 +1,46 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Canvas;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import static java.lang.Character.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Battlefield extends Canvas implements KeyListener, Runnable
+{
+	public Battlefield(){
+		setBackground(Color.black);
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
---
+++ src/SpaceTanks.java
@@ -0,0 +1,39 @@
+import javax.swing.JFrame;
+import javax.swing.JWindow;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class SpaceTanks extends JFrame {
+	private int width;
+	private int height;
+	
+	public SpaceTanks(){
+		super("SPACE TANKS");
+
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setUndecorated(true);
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		width = (int) screenSize.getWidth();
+		height = (int) screenSize.getHeight();
+		
+		System.out.println(width + " " + height);
+		
+		setSize(width, height);
+		
+		Battlefield game = new Battlefield();
+		
+		add(game);
+		
+		setVisible(true);
+	}
+	
+	public static void main( String args[] )
+	{
+		SpaceTanks run = new SpaceTanks();
+	}
+	
+}
